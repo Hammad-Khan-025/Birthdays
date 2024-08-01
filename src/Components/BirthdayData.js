e65ae3612a6abd3086e37762066d0a43597b86dd
Hammad Khan <hk188540@gmail.com>
2024-08-01 11:44:21 +0500

    first commit

---
+++ src/Components/BirthdayData.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: "Hammad Khan",
+        age: 24,
+        image: "my-pic.png"
+    },
+    {
+        id: 2,
+        name: "Zeeshan Habib",
+        age: 25,
+        image: "zeeshan-pic.jpg"
+    },
+    {
+        id: 3,
+        name: "Haider Zahoor",
+        age: 23,
+        image: "haider-pic.jpg"
+    },
+    // {
+    //     id: 4,
+    //     name: "Zain Ali",
+    //     age: 28,
+    //     image: "zain-pic.jpg"
+    // },
+    {
+        id: 5,
+        name: "Hamza Zahid",
+        age: 27,
+        image: "hamza-zahid-pic.jpg"
+    },
+]
